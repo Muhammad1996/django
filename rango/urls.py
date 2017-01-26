3721f450565b08fc98f7d16dc02f66a6cad74420
Muhammad Yousaf <2208505y@student.gla.ac.uk>
2017-01-26 18:41:30 +0000

    Update 8 ch 6 comp

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^about/', views.about, name='about'),
     url(r'^index/',views.index, name='index'),
     url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category'),
     ]
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
 
 # Create your views here.
 from django.http import HttpResponse
 
+def show_category(request, category_name_slug):
+	context_dict={}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		pages = Page.objects.filter(category=category)
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+	return render(request, 'rango/category.html', context_dict)
 
 def index(request):
-	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-	return render(request, 'rango/index.html', context=context_dict)
+	# Query the database for a list of ALL categories currently stored.
+	# Order the categories by no. likes in descending order.
+	# Retrieve the top 5 only - or all if less than 5.
+	# Place the list in our context_dict dictionary
+	# that will be passed to the template engine.
+	page_list = Page.objects.order_by('-views')[:5]
+	category_list= Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list, 'pages':page_list}
+
+
+	# Render the response and send it back
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	context_dict = {'boldmessage': "This tutorial has been put together by Muhammad Yousaf"}
